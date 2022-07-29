1c59f9ecfc3ee20fe24a5178231ad6cc6ba58071
Prince-XD <79126372+Prince-XD@users.noreply.github.com>
2022-07-29 18:02:28 +0530

    Added New Chatbot Api

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ class Config(object):
       BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
       MERISSA_TOKEN = os.environ.get("MERISSA_TOKEN", "")
       OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "NoobxCoder")
+      BOT_NAME = os.environ.get("BOT_NAME", "")
+      OWNER_NAME = os.environ.get("OWNER_NAME", "")
