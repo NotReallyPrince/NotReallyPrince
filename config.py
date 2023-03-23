725095da32f4d508eab6167db8207ea6484b6e02
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2023-03-23 22:41:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-      MERISSA_TOKEN = os.environ.get("MERISSA_TOKEN", "")
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "6103965153:AAEEyo0N_iZfdjyM5jszt5epb27DKGxO6x8")
+      MERISSA_TOKEN = os.environ.get("MERISSA_TOKEN", "2030709195-MERISSAeC3wXV4Iq4")
       OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "NoobxCoder")
-      BOT_NAME = os.environ.get("BOT_NAME", "")
-      OWNER_NAME = os.environ.get("OWNER_NAME", "")
+      BOT_NAME = os.environ.get("BOT_NAME", "DoggyRaid")
+      OWNER_NAME = os.environ.get("OWNER_NAME", "Stapz")
       LANGUAGE_CODE = os.environ.get("LANGUAGE", "lang")
 
    
